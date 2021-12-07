e2aad7a84f7c9b80fdf9aaf9e12deb95dc15ee46
vignesh-v97 <vigneshbharathi34@gmail.com>
2021-12-07 19:27:49 +0530

    validation and array fields implemented

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, Field, ErrorMessage, FieldArray, FastField } from 'formik';
 import * as Yup from 'yup';
 import TextError from './TextError';
 
@@ -12,7 +12,9 @@ const initialValues = {
  social: {
   facebook: '',
   twitter: ''
- }
+ },
+ phoneNumber: ['', ''],
+ phNumbers: [''],
 };
 
 const onSubmit = values => {
@@ -22,20 +24,31 @@ const onSubmit = values => {
 const validationSchema = Yup.object({
  name: Yup.string().required('Required'),
  email: Yup.string().email('Invalid Format').required('Required'),
- channel: Yup.string().required('Required')
+ channel: Yup.string().required('Required'),
 })
+
+const validateComments = value => {
+  let error;
+  if(!value) {
+    error = 'Required';
+  }
+  return error;
+}
 
 function YoutubeForm() {
   return (
   <Formik
    initialValues={initialValues}
    validationSchema={validationSchema}
-   onSubmit={onSubmit}>
+   onSubmit={onSubmit}
+  //  validateOnChange={false}
+  //  validateOnBlur={false}
+   >
    <Form className="form-section">
     <div className="form-control">
     <label htmlFor="name">Name</label>
     <Field type="text" id="name" name="name"/>
-    <ErrorMessage name="name"component={TextError}/>
+    <ErrorMessage name="name" component={TextError} />
     </div>
 
     <div className="form-control">
@@ -52,19 +65,21 @@ function YoutubeForm() {
     <div className="form-control">
     <label htmlFor="channel">Channel</label>
     <Field type="text" id="channel" name="channel"/>
-    <ErrorMessage name="channel"/>
+    <ErrorMessage name="channel" component={TextError}/>
     </div>
     
     <div className="form-control">
      <label htmlFor="comments">Comments</label>
-     <Field as="textarea" id="comments" name="comments"/>
+     <Field as="textarea" id="comments" name="comments" validate={validateComments} />
+     <ErrorMessage name="comments" component={TextError} />
     </div>
 
     <div className="form-control">
      <label htmlFor="address">Address</label>
-     <Field name="address">
+     <FastField name="address">
       {
        (props) => {
+        console.log('Field render')
         const {field, form, meta} = props;
         return (
          <div>
@@ -74,7 +89,7 @@ function YoutubeForm() {
         )
        }
       }
-     </Field>
+     </FastField>
     </div>
 
     <div className="form-control">
@@ -85,6 +100,45 @@ function YoutubeForm() {
     <div className="form-control">
      <label htmlFor="twitter">Twitter</label>
      <Field type="text" id="twitter" name="social.twitter" />
+    </div>
+
+    <div className="form-control">
+     <label htmlFor="primaryPh">Primary Phone Number</label>
+     <Field type="text" id="primaryPh" name="phoneNumber[0]" />
+    </div>
+
+    <div className="form-control">
+     <label htmlFor="secondaryPh">secondary Phone Number</label>
+     <Field type="text" id="secondaryPh" name="phoneNumber[1]" />
+    </div>
+
+    <div className="form-control">
+      <label htmlFor="">List of Phone Numbers</label>
+      <FieldArray name="phNumbers">
+          {
+            (fieldArrayProps) => {
+              const { push, remove,form } = fieldArrayProps;
+              const { values } = form;
+              const { phNumbers } = values;
+              return (
+                <div>
+                  {
+                    phNumbers.map((phNumber, index) => (
+                      <div key={index}>
+                        <Field name={`phNumbers[${index}]`} />
+                        {
+                          (index > 0) && 
+                          <button type="button" onClick={() => remove(index)}>-</button>
+                        }
+                        <button type="button" onClick={() => push('')}>+</button>
+                      </div>
+                    ))
+                  }
+                </div>
+              )
+            }
+          }
+      </FieldArray>
     </div>
 
     <div className="btn">
